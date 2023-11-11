8e35bdf6d967c7b4065089b86e315e1a1e1ad8c5
Michael Gusak <mmll4444@gmail.com>
2023-11-11 00:31:56 -0600

    working vertical collision + jumping

--- Assets/C# Scripts/Actors/Movement.cs
+++ Assets/C# Scripts/Actors/Movement.cs
@@ -7,16 +7,23 @@ using Input;
 
 public class Movement : MonoBehaviour
 {
+    public float JumpForce = 5f;
     public float TerminalVelocity = 10f;
+
+    private float Width;
+    private float Height;
 
     public LayerMask GroundMask;
 
     public float FlooredDistanceThreshold = 0.1f;
     public event EventHandler Floored;
     public event EventHandler FloorExited;
+    private bool FloorCheck;
+    private bool OnFloorLastFrame = false;
 
     private PlayerGameplayInput PlayerInput;
     private Rigidbody2D rb;
+    private BoxCollider2D boxCollider;
     private Global Global;
 
     private void Start()
@@ -25,30 +32,58 @@ public class Movement : MonoBehaviour
         PlayerInput = GetComponent<PlayerGameplayInput>();
         rb = GetComponent<Rigidbody2D>();
         Global = GameObject.Find("Global").GetComponent<Global>();
+        boxCollider = GetComponent<BoxCollider2D>();
 
-        // Subscribe to the JumpButtonPressed event
+        if (boxCollider != null)
+        {
+            // Get the width and height of the collider
+            Width = boxCollider.size.x;
+            Height = boxCollider.size.y;
+        }
+        else Debug.LogError("BoxCollider2D component not found.");
+
+        // Subscribe functions to Events:
         if (PlayerInput != null)
         {
             PlayerInput.JumpButtonPressed += OnJumpButtonPressed;
             PlayerInput.JumpButtonReleased += OnJumpButtonReleased;
         }
-    }
+        else Debug.LogError("PlayerGameplayInput component not found.");
 
-    public void FixedUpdate() 
+        Floored += OnFloor;
+        FloorExited += OnFloorExit;
+
+    }
+    
+    private void FixedUpdate()
     {
         rb.gravityScale = Global.Gravity;
+
+
+
+
+        FloorCheck = CheckForFloor(GroundMask);
+        CheckForRoof(GroundMask); // TODO: make rigidbody y component <= 0
+        ProcessGroundState();
     }
 
     private void OnJumpButtonPressed(object sender, System.EventArgs e)
     {
-        // Handle the jump button pressed event
+        rb.velocity = new Vector2(rb.velocity.x, JumpForce); // TODO: make the jump height tie to held duration?
+        Debug.Log("Jump button pressed!");
+    }
+    private void OnJumpButtonReleased(object sender, System.EventArgs e)
+    {
         Debug.Log("Jump button pressed!");
     }
 
-    private void OnJumpButtonReleased(object sender, System.EventArgs e)
+    private void OnFloor(object sender, System.EventArgs e)
     {
-        // Handle the jump button released event
-        Debug.Log("Jump button pressed!");
+        Debug.Log("Entered Floor");
+    }
+    private void OnFloorExit(object sender, System.EventArgs e)
+    {
+        Debug.Log("Exited Floor");
     }
 
     
@@ -58,6 +93,55 @@ public class Movement : MonoBehaviour
         {
             // Unsubscribe from events
             PlayerInput.JumpButtonPressed -= OnJumpButtonPressed;
+            PlayerInput.JumpButtonReleased -= OnJumpButtonReleased;
         }
+    }
+
+
+
+    private void ProcessGroundState()
+    {
+        if(FloorCheck)
+        {
+            if(!OnFloorLastFrame)
+            {
+                Floored?.Invoke(this, null);
+            }
+            OnFloorLastFrame = true;
+        }
+        else
+        {
+            if(OnFloorLastFrame)
+            {
+                FloorExited?.Invoke(this, null);
+            }
+            OnFloorLastFrame = false;
+        }
+    }
+    
+    private bool CheckForFloor(LayerMask mask)
+    {
+        float x = transform.position.x;
+        float y = transform.position.y;
+
+        Vector2 floorCheckPos = new Vector2(x, y - (Height / 2.0f));
+        Vector2 floorCheckSize = new Vector2(Width, FlooredDistanceThreshold);
+        return ColliderOverlapCheck(floorCheckPos, floorCheckSize, mask);
+    }
+    
+    private bool CheckForRoof(LayerMask mask)
+    {
+        float x = transform.position.x;
+        float y = transform.position.y;
+
+        Vector2 roofPos = new Vector2(x, y + (Height / 2.0f));
+        Vector2 roofSize = new Vector2(Width - FlooredDistanceThreshold, FlooredDistanceThreshold);
+        return ColliderOverlapCheck(roofPos, roofSize, mask);
+    }
+
+    private bool ColliderOverlapCheck(Vector2 position, Vector2 size, LayerMask mask)
+    {
+        Collider2D check = Physics2D.OverlapBox(position, size, 0, mask);
+        return check != null;
     }
 }
